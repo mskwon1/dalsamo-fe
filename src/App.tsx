8226f5003cd602a169b600708c1e9c8d3a7fbe28
mskwon1 <mageeeeek@gmail.com>
2023-04-01 03:54:17 +0900

    Adds react-router and root router

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,23 @@
+import { Row, Col } from 'antd';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
+
+const InnerComponent = () => {
+  return (
+    <Row justify="center" align="middle">
+      <Col xs={24} md={16}>
+        <h1>Hello, DALSAMO-FE!</h1>
+      </Col>
+    </Row>
+  );
+};
+
 function App() {
   return (
-    <div>
-      <h1>Hello, DALSAMO-FE!</h1>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <InnerComponent />
+    </QueryClientProvider>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+const router = createBrowserRouter([{ path: '/', element: <App /> }]);
+
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
