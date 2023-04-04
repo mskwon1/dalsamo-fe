ca2f08beced4a573e46bb6a8fcdf4d861dfa51bf
mskwon1 <mageeeeek@gmail.com>
2023-04-05 01:22:14 +0900

    Changes wr tag

--- src/components/WeeklyReportTag.tsx
+++ src/components/WeeklyReportTag.tsx
@@ -5,10 +5,18 @@ const WeeklyReportTag = (props: { status: 'pending' | 'confirmed' }) => {
   const { status } = props;
 
   if (status === 'pending') {
-    return <Tag icon={<ClockCircleOutlined />}>결산 전</Tag>;
+    return (
+      <Tag color="geekblue-inverse" icon={<ClockCircleOutlined />}>
+        결산 전
+      </Tag>
+    );
   }
 
-  return <Tag icon={<CheckCircleOutlined />}>결산 완료</Tag>;
+  return (
+    <Tag color="green-inverse" icon={<CheckCircleOutlined />}>
+      결산 완료
+    </Tag>
+  );
 };
 
 export default WeeklyReportTag;
