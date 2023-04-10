e8bfbd969fac1f8e9ce16b5cc0912ee367bef514
mskwon1 <mageeeeek@gmail.com>
2023-04-11 02:51:16 +0900

    Adds header

--- src/layouts/BaseLayout.tsx
+++ src/layouts/BaseLayout.tsx
@@ -1,10 +1,16 @@
-import { Col, Layout, Menu, Row, theme } from 'antd';
+import { Grid, Button, Col, Layout, Menu, Row, theme } from 'antd';
 import { Link, Outlet, useLocation } from 'react-router-dom';
 import dalsamoLogo from '../images/dalsamo.png';
-import { BarsOutlined, FileAddOutlined } from '@ant-design/icons';
-import { useMemo } from 'react';
+import {
+  BarsOutlined,
+  FileAddOutlined,
+  MenuFoldOutlined,
+  MenuUnfoldOutlined,
+} from '@ant-design/icons';
+import { useMemo, useState } from 'react';
 import useLoginUser from '@hooks/useLoginUser';
 import _ from 'lodash';
+import useLogout from '@hooks/useLogout';
 
 const MENU_ROUTES = [
   {
@@ -39,6 +45,7 @@ const BaseLayout = () => {
 
   const { pathname } = useLocation();
   const { data: loginUser } = useLoginUser();
+  const logout = useLogout();
 
   const selectedKeys = useMemo(() => {
     const matchedIndexes = [];
@@ -58,57 +65,87 @@ const BaseLayout = () => {
     }
 
     return _.filter(MENU_ITEMS, ({ roles }) => {
-      console.log(roles, loginUser);
-
       return !_.isEmpty(_.intersection(roles, loginUser.roles));
     });
   }, [loginUser]);
 
+  const [collapse, setCollapse] = useState();
+  const { md } = Grid.useBreakpoint();
+
+  console.log({ md });
+
   return (
     <Layout hasSider={true}>
-      <Layout.Sider theme="light" breakpoint="sm" collapsedWidth={0}>
+      <Layout.Sider
+        theme="light"
+        breakpoint="sm"
+        trigger={null}
+        collapsedWidth={0}
+        collapsed={collapse}
+        onCollapse={setCollapse}
+      >
         <div
           style={{ height: '100%', display: 'flex', flexDirection: 'column' }}
         >
-          <div
-            style={{
-              display: 'flex',
-              justifyContent: 'center',
-              alignItems: 'center',
-              marginBottom: 4,
-            }}
-          >
-            <Link to="/">
-              <img
-                src={dalsamoLogo}
-                style={{ width: '100%', height: '100%' }}
-              />
-            </Link>
-          </div>
+          <Link to="/">
+            <img src={dalsamoLogo} style={{ width: '100%', height: '100%' }} />
+          </Link>
           <Menu
             items={filteredMenuItems}
             style={{ flexGrow: 1 }}
             selectedKeys={selectedKeys}
+            onClick={md ? undefined : () => setCollapse(true)}
           />
         </div>
       </Layout.Sider>
-      <Layout.Content style={{ background: colorBgContainer }}>
-        <Row>
-          <Col
-            xs={24}
-            lg={20}
-            xl={18}
-            xxl={16}
-            style={{
-              height: '100vh',
-              background: colorBgContainer,
-              padding: 8,
-            }}
+      <Layout>
+        <Layout.Header
+          style={{
+            position: 'sticky',
+            top: 0,
+            zIndex: 1,
+            background: colorBgContainer,
+            height: 30,
+            display: 'flex',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+            paddingLeft: 8,
+            paddingRight: 8,
+            boxShadow: '0 2px 2px 0 lightgray',
+          }}
+        >
+          {collapse ? (
+            <MenuUnfoldOutlined onClick={() => setCollapse(false)} />
+          ) : (
+            <MenuFoldOutlined onClick={() => setCollapse(true)} />
+          )}
+          <Button
+            size="small"
+            type="text"
+            style={{ fontSize: 12 }}
+            onClick={logout}
           >
-            <Outlet />
-          </Col>
-        </Row>
-      </Layout.Content>
+            로그아웃
+          </Button>
+        </Layout.Header>
+        <Layout.Content style={{ background: colorBgContainer }}>
+          <Row>
+            <Col
+              xs={24}
+              lg={20}
+              xl={18}
+              xxl={16}
+              style={{
+                height: '100vh',
+                background: colorBgContainer,
+                padding: 8,
+              }}
+            >
+              <Outlet />
+            </Col>
+          </Row>
+        </Layout.Content>
+      </Layout>
     </Layout>
   );
 };
